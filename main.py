699ee8df88ffeba97f4289fb5c226684f0031b67
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2024-11-02 03:00:00 +0900

    Implement WebSocket connection manager

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from fastapi import FastAPI, WebSocket
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
+class WebSocketManager:
+    def __init__(self) -> None:
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, ws: WebSocket):
+        await ws.accept()
+        self.connections.append(ws)
+
+    async def disconnect(self, ws: WebSocket):
+        self.connections.remove(ws)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+
+manager = WebSocketManager()
+
 @app.websocket("/")
 async def handler(ws: WebSocket):
-    await ws.accept()
-    while True:
-        data = await ws.receive_text()
-        await ws.send_text(data)
+    await manager.connect(ws)
+    try:
+        while True:
+            data = await ws.receive_text()
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        await manager.disconnect(ws)
