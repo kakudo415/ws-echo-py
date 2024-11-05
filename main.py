dc856a90c3ffd2b30b6ca8cbcd96c6ce1fe566d8
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2024-11-05 12:00:52 +0900

    Implement matching queue

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from typing import List
+from queue import Queue
+from asyncio import sleep, create_task
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
-app = FastAPI()
 
-
-class WebSocketManager:
+class RoomManager:
     def __init__(self):
         self.connections: List[WebSocket] = []
+        self.matching = Queue()
 
     async def connect(self, ws: WebSocket):
         await ws.accept()
         self.connections.append(ws)
+
+    async def join(self, ws: WebSocket):
+        await ws.send_json({"status": "matching"})
+        self.matching.put(ws)
+
+    async def match(self):
+        while True:
+            if self.matching.qsize() >= 2:
+                ws1 = self.matching.get()
+                ws2 = self.matching.get()
+                await ws1.send_json({"status": "matched"})
+                await ws2.send_json({"status": "matched"})
+            await sleep(1)
 
     async def disconnect(self, ws: WebSocket):
         self.connections.remove(ws)
@@ -21,12 +36,22 @@ class WebSocketManager:
             await connection.send_json(data)
 
 
-manager = WebSocketManager()
+manager = RoomManager()
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    create_task(manager.match())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.websocket("/")
 async def handler(ws: WebSocket):
     await manager.connect(ws)
+    await manager.join(ws)
     try:
         while True:
             req = await ws.receive_json()
