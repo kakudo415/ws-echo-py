24967c6ddda08c03b9af7199feb651764ff9a20e
KAKUDO Kentaro <31888089+kakudo415@users.noreply.github.com>
2024-11-05 11:14:12 +0900

    Implement JSON parsing

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
+
 class WebSocketManager:
-    def __init__(self) -> None:
+    def __init__(self):
         self.connections: List[WebSocket] = []
 
     async def connect(self, ws: WebSocket):
@@ -15,18 +16,20 @@ class WebSocketManager:
     async def disconnect(self, ws: WebSocket):
         self.connections.remove(ws)
 
-    async def broadcast(self, data: str):
+    async def broadcast(self, data: dict):
         for connection in self.connections:
-            await connection.send_text(data)
+            await connection.send_json(data)
+
 
 manager = WebSocketManager()
+
 
 @app.websocket("/")
 async def handler(ws: WebSocket):
     await manager.connect(ws)
     try:
         while True:
-            data = await ws.receive_text()
-            await manager.broadcast(data)
+            req = await ws.receive_json()
+            await manager.broadcast(req)
     except WebSocketDisconnect:
         await manager.disconnect(ws)
